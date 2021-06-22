4dee3d8496b2a59f1942707d885bec9c21eae817
woodi97 <woodi.daily@gmail.com>
2021-06-23 03:27:05 +0900

    Remove Unused Pages

--- src/pages/intro.tsx
+++ src/pages/intro.tsx
@@ -1,9 +1,7 @@
 import { Contact, Footer, Menu } from "@src/components/common";
 import React, { useEffect } from "react";
 
-export interface ILandingProps {}
-
-export default function Landing(props: ILandingProps) {
+export default function Landing(): JSX.Element {
 	return (
 		<>
 			<div id="wrapper"></div>
