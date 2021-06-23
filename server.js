799d478306a92ac096506bd303f388139e0a3271
woodi97 <woodi.daily@gmail.com>
2021-06-23 13:46:54 +0900

    Add SocketIO Setting

--- server.js
+++ server.js
@@ -7,10 +7,28 @@ const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
+const config = require("./config");
+const { ExpressPeerServer } = require("peer");
 
 app.prepare().then(() => {
 	const server = new Koa();
 	const router = new Router();
+	const io = require("socket.io")(server);
+
+	const peerServer = ExpressPeerServer(server, {
+		allow_discovery: true,
+		debug: true,
+	});
+
+	server.use(peerServer);
+
+	router.get("/signin", async (ctx) => {
+		await app.render(ctx.req, ctx.res, "/", ctx.req.query);
+	});
+
+	router.get("/signup", async (ctx) => {
+		await app.render(ctx.req, ctx.res, "/", ctx.req.query);
+	});
 
 	router.get("/", async (ctx) => {
 		await app.render(ctx.req, ctx.res, "/", ctx.query);
@@ -22,6 +40,22 @@ app.prepare().then(() => {
 		ctx.respond = false;
 	});
 
+	// io connection for video/voice chat
+	io.on("connection", (socket) => {
+		socket.on("join-room", (roomId, userId, emailId, profileImage) => {
+			socket.join(roomId);
+			socket.broadcast
+				.to(roomId)
+				.emit("user-connected", userId, emailId, profileImage);
+			socket.on("disconnect", () => {
+				socket.broadcast.to(roomId).emit("user-disconnected", userId);
+			});
+			socket.on("leave-room", (userId) => {
+				socket.broadcast.to(roomId).emit("user-leave", userId);
+			});
+		});
+	});
+
 	server.use(async (ctx, next) => {
 		ctx.res.statusCode = 200;
 		await next();
@@ -29,6 +63,6 @@ app.prepare().then(() => {
 
 	server.use(router.routes());
 	server.listen(port, () => {
-		console.log(`> Ready on http://localhost:${port}`);
+		console.log(`> Ready on ${config.apiConfig.baseURL}`);
 	});
 });
