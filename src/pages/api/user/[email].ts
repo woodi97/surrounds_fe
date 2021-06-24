b9da14eb2e9e6fc655e794337766b2678bd7f831
woodi97 <woodi.daily@gmail.com>
2021-06-24 17:55:17 +0900

    Add User LookUp API

--- src/pages/api/user/[email].ts
+++ src/pages/api/user/[email].ts
@@ -1,7 +1,51 @@
-export default (req, res) => {
+import { NextApiRequest, NextApiResponse } from "next";
+import nextConnect from "next-connect";
+import User from "@src/models/user";
+import connectDB from "@src/util/mongodb";
+import authMiddleware from "@src/util/auth";
+import config from "../../../../config";
+
+interface Request extends NextApiRequest {
+	params: any;
+	decoded: any;
+}
+
+const api = nextConnect<Request, NextApiResponse>();
+
+api.use(authMiddleware);
+
+api.get(async (req, res) => {
 	const {
 		query: { email },
 	} = req;
+	const check = (user) => {
+		if (!user) {
+			//user does not exist
+			throw new Error("No matching email");
+		}
+		return user;
+	};
 
-	res.end(`Post: ${email}`);
-};
+	const respond = (user) => {
+		res.json({
+			header: {
+				message: "success",
+			},
+			body: {
+				email: user.email,
+				nickname: user.nickname,
+				profileImage: config.apiConfig.IMAGE_URL + user.profileImage,
+			},
+		});
+	};
+
+	const onError = (error) => {
+		res.status(403).json({
+			message: error.message,
+		});
+	};
+
+	User.findOneByEmail(email).then(check).then(respond).catch(onError);
+});
+
+export default connectDB(api);
--- src/pages/api/user/index.ts
+++ src/pages/api/user/index.ts
@@ -1,11 +1,50 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import nextConnect from "next-connect";
+import User from "@src/models/user";
 import connectDB from "@src/util/mongodb";
+import authMiddleware from "@src/util/auth";
+import config from "../../../../config";
 
-const api = nextConnect<NextApiRequest, NextApiResponse>();
+interface Request extends NextApiRequest {
+	params: any;
+	decoded: any;
+}
 
-api.post(async (req, res) => {
-	res.json("success");
+const api = nextConnect<Request, NextApiResponse>();
+
+api.use(authMiddleware);
+
+api.get(async (req, res) => {
+	const userE = req.params.email ? req.params.email : req.decoded["email"];
+
+	const check = (user) => {
+		if (!user) {
+			//user does not exist
+			throw new Error("No matching email");
+		}
+		return user;
+	};
+
+	const respond = (user) => {
+		res.json({
+			header: {
+				message: "success",
+			},
+			body: {
+				email: user.email,
+				nickname: user.nickname,
+				profileImage: config.apiConfig.IMAGE_URL + user.profileImage,
+			},
+		});
+	};
+
+	const onError = (error) => {
+		res.status(403).json({
+			message: error.message,
+		});
+	};
+
+	User.findOneByEmail(userE).then(check).then(respond).catch(onError);
 });
 
 export default connectDB(api);
