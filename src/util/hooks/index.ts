c183ba7707697c9b13fb26c6059cab1880dc9452
woodi <woodi.daily@gmail.com>
2021-06-29 03:07:37 +0900

    Add UserMedia Hooks

--- src/util/hooks/index.ts
+++ src/util/hooks/index.ts
@@ -1,1 +1,2 @@
 export { default as useLocation } from "./useLocation";
+export { default as useUserMedia } from "./useUserMedia";
--- src/util/hooks/useLocation.ts
+++ src/util/hooks/useLocation.ts
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Location } from "@src/core/interface";
 
 const gpsOptions = {
