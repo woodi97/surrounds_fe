5513eec1add9c1da356b9d10ad310e58724ddef3
woodi <woodi.daily@gmail.com>
2021-06-29 02:46:08 +0900

    Add Chatroom

---
+++ src/components/primary/Room/Room.tsx
@@ -0,0 +1,303 @@
+import React, { useCallback, useContext, useEffect, useState } from "react";
+import router from "next/router";
+// import modules
+import Peer from "peerjs";
+import classNames from "classnames";
+import { SocketContext } from "@src/util/socket";
+// import interface
+import { Location, RoomInfo } from "@src/core/interface";
+// import component
+import RoomMedia from "./RoomMedia";
+// import config
+import config from "@src/core/config";
+// import api
+import { deleteChatroom } from "@src/core/api/chatroom";
+// import styles
+import styles from "./Room.module.scss";
+
+interface IRoomProps {
+	className?: string;
+	chatroom?: RoomInfo;
+	emailId: string;
+	profileImage: string;
+	currentLocation: Location;
+	getChatRooms(location: Location): void;
+	onClick(emailId: string, e: any): void;
+}
+
+interface LocalMediaInfo {
+	peerId: string;
+	emailId: string;
+	profileImage: string;
+	stream: MediaStream;
+}
+
+export default function Room(props: IRoomProps): JSX.Element {
+	const {
+		className,
+		chatroom,
+		emailId,
+		profileImage,
+		currentLocation,
+		getChatRooms,
+		onClick,
+	} = props;
+	const peers = {};
+	const socket = useContext(SocketContext);
+	const [myPeer, setPeer] = useState<Peer>(undefined);
+	const [localMediaInfo, setLocalMediaInfo] = useState<LocalMediaInfo>(null);
+	const [remoteMediaInfos, setRemoteMediaInfos] = useState<LocalMediaInfo[]>(
+		[],
+	);
+
+	const cleanUp = () => {
+		if (myPeer) {
+			myPeer.disconnect();
+			// myPeer.destroy();
+			// myPeer.off("call", () => {
+			// 	console.log("call event disarmed");
+			// });
+			// myPeer.off("close", () => {
+			// 	console.log("close event disarmed");
+			// });
+			// myPeer.off("disconnected", () => {
+			// 	console.log("disconnected event disarmed");
+			// });
+			// myPeer.off("error", () => {
+			// 	console.log("error event disarmed");
+			// });
+			// socket.off("user-connected", () => {
+			// 	console.log("user-connected event closed");
+			// });
+			// socket.off("user-disconnected", () => {
+			// 	console.log("user-disconnected event closed");
+			// });
+			// socket.off("user-leave", () => {
+			// 	console.log("user-leave event closed");
+			// });
+		}
+		setPeer(null);
+	};
+
+	const addRemoteStream = useCallback(
+		(stream, peerId, emailId, profileImage) => {
+			setRemoteMediaInfos((remoteMediaInfos) => {
+				if (!stream || !peerId) return [...remoteMediaInfos];
+				if (
+					remoteMediaInfos.some(
+						(remoteMediaInfo) => remoteMediaInfo.peerId === peerId,
+					)
+				)
+					return [...remoteMediaInfos];
+				return [
+					...remoteMediaInfos,
+					{
+						peerId: peerId,
+						emailId: emailId,
+						stream: stream,
+						profileImage: profileImage,
+					},
+				];
+			});
+		},
+		[],
+	);
+
+	const removeRemoteStream = useCallback((peerId) => {
+		setRemoteMediaInfos((remoteMediaInfos) => {
+			const index = remoteMediaInfos.findIndex(
+				(remote) => remote.peerId === peerId,
+			);
+			if (index < 0) return [...remoteMediaInfos];
+			remoteMediaInfos.splice(index, 1);
+			return [...remoteMediaInfos];
+		});
+	}, []);
+
+	useEffect(() => {
+		if (localMediaInfo) {
+			import("peerjs").then(({ default: Peer }) => {
+				const peer = myPeer ? myPeer : new Peer(undefined, config.peerConfig);
+				setPeer(peer);
+
+				peer.on("open", (peerId) => {
+					socket.emit("join-room", chatroom.id, peerId, emailId, profileImage);
+				});
+
+				peer.on("call", (call) => {
+					const { emailId, profileImage } = call.metadata;
+					console.log("get call from ", call.peer, emailId, profileImage);
+					call.answer(localMediaInfo.stream);
+					call.on("stream", (remoteStream) => {
+						addRemoteStream(remoteStream, call.peer, emailId, profileImage);
+					});
+
+					call.on("close", () => {
+						removeRemoteStream(call.peer);
+					});
+
+					call.on("error", (error) => {
+						console.log(error);
+						removeRemoteStream(call.peer);
+					});
+				});
+
+				peer.on("close", () => {
+					console.log("Peer closed remotetly");
+					cleanUp();
+				});
+
+				peer.on("disconnected", () => {
+					console.log("Peer disconnected");
+					cleanUp();
+				});
+
+				peer.on("error", (error) => {
+					console.log("peer error", error);
+					cleanUp();
+				});
+
+				// caller section
+				socket.on(
+					"user-connected",
+					(userId, othersEmail, othersProfileImage) => {
+						//call.peer == userId
+						console.log(
+							"connected user has : ",
+							userId,
+							othersEmail,
+							othersProfileImage,
+						);
+						if (peer.disconnected) {
+							peer.connect(userId);
+						} else {
+							const call = peer.call(userId, localMediaInfo?.stream, {
+								metadata: {
+									emailId: localMediaInfo?.emailId,
+									profileImage: localMediaInfo?.profileImage,
+								},
+							});
+							peers[userId] = call;
+							call.on("stream", (remoteStream) => {
+								addRemoteStream(
+									remoteStream,
+									call.peer,
+									othersEmail,
+									othersProfileImage,
+								);
+								console.log("connected to" + call.peer);
+							});
+							call.on("disconnected", () => {
+								console.log("disconnected");
+							});
+							call.on("close", () => {
+								console.log("call closed");
+								removeRemoteStream(call.peer);
+								call.close();
+							});
+							call.on("error", (error) => {
+								console.log("call error", error);
+								removeRemoteStream(call.peer);
+								call.close();
+							});
+						}
+					},
+				);
+				socket.on("user-disconnected", (userId) => {
+					console.log("user-disconnected to");
+					removeRemoteStream(userId);
+					if (peers[userId]) peers[userId].close();
+				});
+				socket.on("user-leave", (userId) => {
+					console.log(userId);
+					removeRemoteStream(userId);
+					if (peers[userId]) peers[userId].close();
+				});
+			});
+		}
+		return () => {
+			cleanUp();
+		};
+	}, [localMediaInfo]);
+
+	const getMyMediaStream = async () => {
+		const myMedia = await navigator.mediaDevices.getUserMedia({
+			video: false,
+			audio: { sampleSize: 8, echoCancellation: true },
+		});
+		setLocalMediaInfo({
+			peerId: "temp",
+			emailId: emailId,
+			stream: myMedia,
+			profileImage: profileImage,
+		});
+	};
+
+	useEffect(() => {
+		getMyMediaStream();
+	}, []);
+
+	const onExitButtonClick = async (e) => {
+		e.preventDefault();
+		if (remoteMediaInfos.length === 0) {
+			try {
+				const result = await deleteChatroom(chatroom.generator.email);
+				console.log(result);
+				socket.emit("leave-room", myPeer.id);
+				myPeer.disconnect();
+				myPeer.destroy();
+				getChatRooms(currentLocation);
+				router.push("/", undefined, { shallow: true });
+			} catch (error) {
+				console.log(error);
+			}
+		} else {
+			socket.emit("leave-room", myPeer.id);
+			myPeer.disconnect();
+			myPeer.destroy();
+			getChatRooms(currentLocation);
+			router.push("/", undefined, { shallow: true });
+		}
+	};
+
+	return (
+		<div className={classNames(className, styles.container)}>
+			<div className={styles.header}>
+				<div className={styles.header_infos}>
+					<div className={styles.header_infos_title}>{chatroom.title}</div>
+					<div className={styles.header_infos_location}>
+						{/* 흑석로, 중앙대학교(5m) */}
+					</div>
+				</div>
+				<div className={styles.header_buttons}>
+					<img
+						onClick={onExitButtonClick}
+						src="/images/icon_exit.svg"
+						alt=""
+					></img>
+				</div>
+			</div>
+			<div className={styles.participants}>
+				<RoomMedia
+					mediaStream={localMediaInfo?.stream}
+					emailId={emailId}
+					onClick={onClick}
+					muted={true}
+					profileImage={localMediaInfo?.profileImage}
+				/>
+				{remoteMediaInfos.map((remoteMediaInfo, index) => {
+					return (
+						<RoomMedia
+							key={index}
+							mediaStream={remoteMediaInfo.stream}
+							emailId={remoteMediaInfo.emailId}
+							onClick={onClick}
+							muted={false}
+							profileImage={remoteMediaInfo.profileImage}
+						/>
+					);
+				})}
+			</div>
+		</div>
+	);
+}
---
+++ src/components/primary/Room/RoomMedia.tsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useRef } from "react";
+
+import classNames from "classnames";
+import styles from "./RoomMedia.module.scss";
+
+interface IRoomMediaProps {
+	className?: string;
+	mediaStream: MediaStream;
+	emailId: string;
+	muted: boolean;
+	profileImage: string;
+	onClick(emailId: string, e: any): void;
+}
+
+export default function RoomMedia(props: IRoomMediaProps): JSX.Element {
+	const { className, mediaStream, emailId, profileImage, muted, onClick } =
+		props;
+	const viewRef = useRef<HTMLVideoElement>(null);
+
+	useEffect(() => {
+		console.log("profileImage is ", profileImage);
+		if (!viewRef.current) return;
+		viewRef.current.srcObject = mediaStream ? mediaStream : null;
+	}, [mediaStream]);
+
+	// useEffect(() => {
+	// 	function drawLoudness() {
+	// 		requestAnimationFrame(drawLoudness);
+	// 		const bufferLength = audioAnalyser.frequencyBinCount;
+	// 		const dataArray = new Uint8Array(bufferLength);
+	// 		audioAnalyser.getByteFrequencyData(dataArray);
+	// 		const audioLevel =
+	// 			dataArray.reduce((p, c) => p + c, 0) / dataArray.length;
+	// 		if (audioLevel > 50) {
+	// 			viewRef.current.style.borderColor = "green";
+	// 		} else {
+	// 			if (viewRef.current) viewRef.current.style.borderColor = "transparent";
+	// 		}
+	// 	}
+	// 	const audioContext = new AudioContext();
+	// 	const audioAnalyser = new AnalyserNode(audioContext, { fftSize: 32 });
+	// 	if (mediaStream) {
+	// 		const audioSource = audioContext.createMediaStreamSource(mediaStream);
+	// 		audioSource.connect(audioAnalyser);
+	// 		audioSource.connect(audioContext.destination);
+	// 		drawLoudness();
+	// 	}
+	// }, [mediaStream]);
+
+	return (
+		<div className={classNames(className, styles.container)}>
+			<video
+				className={styles.video}
+				ref={viewRef}
+				autoPlay
+				playsInline
+				muted={muted}
+				poster={
+					profileImage === "https://boundary.or.kr/api/NULL"
+						? "/default_user.png"
+						: profileImage
+				}
+				onClick={(e) => {
+					onClick(emailId, e);
+				}}
+			/>
+			<div className={styles.label}>{emailId ? emailId : "unknown"}</div>
+		</div>
+	);
+}
