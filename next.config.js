731fe20d141898e10aff934a95a4e82474df6a9a
woodi <woodi.daily@gmail.com>
2021-06-29 23:54:02 +0900

    Add Runtime process.env Configuration

--- next.config.js
+++ next.config.js
@@ -5,4 +5,21 @@ module.exports = {
 	sassOptions: {
 		includePaths: [path.join(__dirname, "styles")],
 	},
+	// Add Runtime process.env Configuration
+	serverRuntimeConfig: {
+		// Will only be available on the server side
+		staticFolder: "/public",
+	},
+	publicRuntimeConfig: {
+		// Will be available on both server and client
+		stunURL: process.env.STUN_URL,
+		turnURL: process.env.TURN_URL,
+		turnUsername: process.env.TURN_USERNAME,
+		turnCredential: process.env.TURN_CREDENTIAL,
+		peerHost: process.env.PEER_HOST,
+		peerPort: process.env.PEER_PORT,
+		peerDebug: process.env.PEER_DEBUG,
+		peerPath: process.env.PEER_PATH,
+		peerSecure: process.env.PEER_SECURE,
+	},
 };
