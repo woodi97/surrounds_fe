cc1b0c83580eb73e32012855d2ef833d636bdfdd
woodi <woodi.daily@gmail.com>
2021-07-16 00:58:48 +0900

    Add Material UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,6 +15,14 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
 			<Head>
 				<title>Surrounds</title>
 				<link rel="icon" href="/images/favicon.ico" />
+				<link
+					rel="stylesheet"
+					href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+				/>
+				<link
+					rel="stylesheet"
+					href="https://fonts.googleapis.com/icon?family=Material+Icons"
+				/>
 				<meta charSet="utf-8" />
 				<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
 				<meta
