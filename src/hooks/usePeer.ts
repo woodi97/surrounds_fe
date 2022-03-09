ae6f17945233c499ed753d94d159b6d148a26f16
woodi <woodi.daily@gmail.com>
2022-03-09 11:27:00 +0900

    Refactor: PeerConnection Code

--- src/hooks/usePeer.ts
+++ src/hooks/usePeer.ts
@@ -23,71 +23,86 @@ const usePeer = ({ addRemoteStream, removeRemoteStream, chatroom, localStream })
     }
   }
 
+  const addGetNewUserEvent = ({ peer }) => {
+    // got new user
+    socket.on('user-connected', (userId) => {
+      console.log('user-connected', userId)
+      const call = peer.call(userId, localStream, {
+        metadata: {
+          username: 'test',
+          profile: 'test',
+        },
+      })
+      peers[userId] = call
+      call.on('stream', (remoteStream) => {
+        addRemoteStream({ peerId: userId, stream: remoteStream })
+      })
+      call.on('disconnected', () => {
+        console.log('disconnected')
+      })
+      call.on('close', () => {
+        removeRemoteStream(userId)
+        call.close()
+      })
+      call.on('error', () => {
+        removeRemoteStream(userId)
+        call.close()
+      })
+    })
+    socket.on('leave-room', (userId) => {
+      console.log(userId)
+      peers[userId]?.close()
+      removeRemoteStream(userId)
+    })
+  }
+
+  const addConnectToChatroomEvent = ({ peer }: { peer: Peer }) => {
+    peer.on('open', () => {
+      peerInstance.current = peer
+      peerID.current = peer.id
+      socket.emit('join-room', chatroom.id, peer.id)
+    })
+  }
+
+  const addConnectToOtherPeersEvent = ({ peer }: { peer: Peer }) => {
+    peer.on('call', (call) => {
+      const { username, profile } = call.metadata
+      call.answer(localStream)
+      call.on('stream', (remoteStream) => {
+        addRemoteStream({ peerId: call.peer, stream: remoteStream })
+      })
+      call.on('close', () => {
+        console.log('The call has ended')
+        removeRemoteStream(call.peer)
+      })
+      call.on('error', (error) => {
+        console.log(error)
+        removeRemoteStream(call.peer)
+      })
+    })
+  }
+
+  const addDisconnectToPeerServerEvent = ({ peer }: { peer: Peer }) => {
+    peer.on('disconnected', () => {
+      console.log('disconnected with peer server')
+      socket.emit('leave-room', peerID.current, chatroom.id)
+    })
+    peer.on('close', () => {
+      console.log('close connection with peer server')
+    })
+    peer.on('error', (error) => {
+      console.log('error event from peer server', error)
+    })
+  }
+
   useEffect(() => {
     if (localStream) {
       import('peerjs').then(({ default: Peer }) => {
         const peer = new Peer(String(getRandomId()), peerConfig)
-        peer.on('open', () => {
-          peerInstance.current = peer
-          peerID.current = peer.id
-          socket.emit('join-room', chatroom.id, peer.id)
-        })
-        peer.on('call', (call) => {
-          const { username, profile } = call.metadata
-          call.answer(localStream)
-          call.on('stream', (remoteStream) => {
-            addRemoteStream({ peerId: call.peer, stream: remoteStream })
-          })
-          call.on('close', () => {
-            console.log('The call has ended')
-            removeRemoteStream(call.peer)
-          })
-          call.on('error', (error) => {
-            console.log(error)
-            removeRemoteStream(call.peer)
-          })
-        })
-        peer.on('disconnected', () => {
-          console.log('disconnected with peer server')
-          socket.emit('leave-room', peerID.current, chatroom.id)
-        })
-        peer.on('close', () => {
-          console.log('close connection with peer server')
-        })
-        peer.on('error', (error) => {
-          console.log('error event from peer server', error)
-        })
-
-        // got new user
-        socket.on('user-connected', (userId) => {
-          console.log('user-connected', userId)
-          const call = peer.call(userId, localStream, {
-            metadata: {
-              username: 'test',
-              profile: 'test',
-            },
-          })
-          peers[userId] = call
-          call.on('stream', (remoteStream) => {
-            addRemoteStream({ peerId: userId, stream: remoteStream })
-          })
-          call.on('disconnected', () => {
-            console.log('disconnected')
-          })
-          call.on('close', () => {
-            removeRemoteStream(userId)
-            call.close()
-          })
-          call.on('error', () => {
-            removeRemoteStream(userId)
-            call.close()
-          })
-        })
-        socket.on('leave-room', (userId) => {
-          console.log(userId)
-          peers[userId]?.close()
-          removeRemoteStream(userId)
-        })
+        addConnectToChatroomEvent({ peer })
+        addConnectToOtherPeersEvent({ peer })
+        addDisconnectToPeerServerEvent({ peer })
+        addGetNewUserEvent({ peer })
       })
     }
     return () => {
